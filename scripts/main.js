f93b10446f9539cb47647d376dd10819e17e93f5
Raul Belloso M <raul.belloso.m@gmail.com>
2022-06-12 12:25:37 -0500

    update (tacos) seccion responsiva, imagenes, descripcion y titulo dinamicos

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-      $(".grid-menu-tacos .box-tacos").hover(
-        function(){
-            const title = $(this).find('p').text();
-            $("#taco-title").text(title);
-            $("#taco-description").text(`Descripción del ${title}`);
-        }    
-    );
+$(document).ready(function () {
+  $(".grid-menu-tacos .box-tacos").hover(function () {
+    const title = $(this).find("p").text();
+    const description = $(this).find("h6").text();
+    const img = $(this).find("img");
+    $("#taco-title").text(title);
+    $("#taco-description").text(description);
+    $("#taco-img").attr("src", img[0].src);
   });
+});
