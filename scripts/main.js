22dec849075307181ece4eb92b30b4e875498403
Raul Belloso M <raul.belloso.m@gmail.com>
2022-06-30 17:12:20 -0500

    fix gorditas y quesadillas animation

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,25 @@ $(document).ready(function () {
     $("#taco-img").attr("src", img[0].src);
   });
 });
+
+function reveal() {
+  var reveals = document.querySelectorAll(".card-quesadillas-slide-left");
+  var reveals2 = document.querySelectorAll(".card-quesadillas-slide-right");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementTop = reveals2[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+      reveals2[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+      reveals2[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
