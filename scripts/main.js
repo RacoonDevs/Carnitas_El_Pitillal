f59538e724b83710eb9e28b45865e86c350347a5
Raul Belloso M <raul.belloso.m@gmail.com>
2022-06-30 17:26:32 -0500

    fix gorditas y quesadillas animation

--- scripts/main.js
+++ scripts/main.js
@@ -11,22 +11,34 @@ $(document).ready(function () {
 
 function reveal() {
   var reveals = document.querySelectorAll(".card-quesadillas-slide-left");
-  var reveals2 = document.querySelectorAll(".card-quesadillas-slide-right");
 
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementTop = reveals2[i].getBoundingClientRect().top;
     var elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
-      reveals2[i].classList.add("active");
     } else {
       reveals[i].classList.remove("active");
-      reveals2[i].classList.remove("active");
+    }
+  }
+}
+function reveal2() {
+  var reveals = document.querySelectorAll(".card-quesadillas-slide-right");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
 }
 
 window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal2);
