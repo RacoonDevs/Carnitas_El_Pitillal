9124546880b30aebc2fb16d9d133e8c5593cb8bf
Raul Belloso M <raul.belloso.m@gmail.com>
2022-06-30 16:25:28 -0500

    change main fotos

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,12 +1,17 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navbarItem = document.getElementsByClassName("navbar-item")[0];
+const navbar = document.getElementsByClassName("navbar")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 navbarItem.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
+});
+navbar.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  // navbarLinks.classList.toggle("active");
 });
 
 window.addEventListener("scroll", function () {
